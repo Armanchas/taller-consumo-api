7be2a54cfb3e7cce36168647db2422d3d2e0ed02
Armin Hucke <arminhucke@protonmail.com>
2024-10-09 15:31:15 -0300

    feat: add button to make api call and log to console

---
+++ src/components/Buttons.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { fetchPopulationData } from "@/services/countryData.js";
+
+const handleCountryClick = async () => {
+  try {
+    const data = await fetchPopulationData();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+</script>
+
+<template>
+  <div class="flex justify-center py-20">
+    <button class="bg-green-300 hover:bg-green-200" @click="handleCountryClick">
+      Fetch Country Data
+    </button>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
+import Buttons from '@/components/Buttons.vue';
 </script>
 
 <template>
   <main>
+    <Buttons />
   </main>
 </template>
